93e56a142d2d9ac9322e1bc8ff419436240e4ec3
Zeroga98 <sebastianrojasgarzon@hotmail.com>
2021-09-03 01:26:06 -0500

    feat(cardContinent): create component

---
+++ src/components/cardContinent/cardContinent.tsx
@@ -0,0 +1,29 @@
+
+import { Dispatch, SetStateAction } from 'react';
+import LogoContinent from "../logoContinent/logoContinent"
+
+
+function CardContinent(continent: IContinent, continent_name: string, dropdownOpen: string, setDropdownOpen: Dispatch<SetStateAction<string>>) {
+    return (
+        <div className={`item-continent item-continent--${continent_name.split(" ")[0]} ${dropdownOpen === continent_name ? 'item-continent--open' : 'item-continent--hiden'} `}>
+            <div className="picture align-self-center">
+                <LogoContinent continent={continent_name.split(" ")[0]} />
+            </div>
+            <div className="detail align-self-center">
+                <span className="detail__title">Región Geografica</span>
+                <span className="name-continent">{continent_name}</span>
+                <li className="detail__drop dropdown nav-item" onClick={() => {
+                    if (dropdownOpen === continent_name) {
+                        setDropdownOpen("default")
+                    } else {
+                        setDropdownOpen(continent_name)
+                    }
+                }}>
+                    <a className="dropdown-toggle nav-link">Ver Paises</a>
+                </li>
+            </div>
+        </div>
+    )
+}
+
+export default CardContinent
